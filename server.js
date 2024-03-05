896957794200cc6a6680e6e079b54c5e71de9c40
okay-head <68184623+okay-head@users.noreply.github.com>
2024-03-05 16:29:36 +0530

    cors is a pain

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const cors = require('cors')
 
 app.use(
 	cors({
-		origin: 'http://localhost:5173/',
+		// origin: ['http://localhost:5173/', 'https://taskcentral.netlify.app/'],
+		origin: '*',
 		methods: ['GET', 'POST', 'PATCH', 'DELETE'],
 		allowedHeaders: ['Content-Type'],
 	})

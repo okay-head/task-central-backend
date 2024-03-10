f2cc5e7242b4d99f2e2e5ccddd416950bee92db0
okay-head <68184623+okay-head@users.noreply.github.com>
2024-03-10 12:04:59 +0530

    chore: add notes

--- model/Task.js
+++ model/Task.js
@@ -10,4 +10,8 @@ const taskSchema = new mongoose.Schema(
 	{ timestamps: true }
 )
 
+// note that the collection would be created as follows
+// Name of the model, lowercased -> task
+// Plural, since it is a collection of docs -> tasks
+
 module.exports = mongoose.model('Task', taskSchema)
